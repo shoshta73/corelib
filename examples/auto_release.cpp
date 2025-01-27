53e1bff3b545a9471b181ff35dd9089583ad519d
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2025-01-27 17:33:47 +0100

    added autorelease object

---
+++ examples/auto_release.cpp
@@ -0,0 +1,23 @@
+#include <corelib.hpp>
+
+// we are simulating c api here
+
+int *create_int(int value)
+{
+  corelib::log::info("create_int: {}", value);
+  return new int(value);
+}
+
+void destroy_int(int *ptr)
+{
+  corelib::log::info("destroy_int: {}", *ptr);
+  delete ptr;
+}
+
+int main()
+{
+  auto ptr = corelib::AutoRelease<int *>(create_int(42), destroy_int);
+
+  auto foo = corelib::AutoRelease<int *>(); // empty
+  foo = corelib::AutoRelease<int *>(create_int(24), destroy_int);
+}
--- include/corelib.hpp
+++ include/corelib.hpp
@@ -6,6 +6,7 @@
 
 #include <corelib/typedef.hpp>
 #include <corelib/def.hpp>
+#include <corelib/auto_release.hpp>
 
 #include <corelib/assert.hpp>
 #include <corelib/log.hpp>
