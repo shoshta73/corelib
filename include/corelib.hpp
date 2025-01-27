5e1664c3bd79235a8385c776f245a3a3061adda0
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2025-01-27 12:55:22 +0100

    add typedefs and mins and maxs

--- include/corelib.hpp
+++ include/corelib.hpp
@@ -2,6 +2,8 @@
 #define __CORELIB_HPP__
 
 #include <corelib/hello.hpp> // TODO: remove
+#include <corelib/typedef.hpp>
+#include <corelib/def.hpp>
 #include <corelib/assert.hpp>
 #include <corelib/instrumentor.hpp>
 #include <corelib/log.hpp>
---
+++ include/corelib/typedef.hpp
@@ -0,0 +1,60 @@
+#ifndef __CORELIB_TYPEDEF_HPP__
+#define __CORELIB_TYPEDEF_HPP__ 1
+
+namespace corelib {
+
+  using uint8_t = unsigned char;
+  using uint16_t = unsigned short;
+  using uint32_t = unsigned int;
+  using uint64_t = unsigned long;
+
+  using int8_t = signed char;
+  using int16_t = signed short;
+  using int32_t = signed int;
+  using int64_t = signed long;
+
+  using size_t = unsigned long;
+  using usize_t = size_t;
+  using ssize_t = signed long;
+
+  using u8_t = uint8_t;
+  using u16_t = uint16_t;
+  using u32_t = uint32_t;
+  using u64_t = uint64_t;
+
+  using s8_t = int8_t;
+  using i8_t = int8_t;
+  using s16_t = int16_t;
+  using i16_t = int16_t;
+  using s32_t = int32_t;
+  using i32_t = int32_t;
+  using s64_t = int64_t;
+  using i64_t = int64_t;
+
+  using uint8 = uint8_t;
+  using uint16 = uint16_t;
+  using uint32 = uint32_t;
+  using uint64 = uint64_t;
+
+  using int8 = int8_t;
+  using int16 = int16_t;
+  using int32 = int32_t;
+  using int64 = int64_t;
+
+  using u8 = uint8;
+  using u16 = uint16;
+  using u32 = uint32;
+  using u64 = uint64;
+
+  using s8 = int8;
+  using i8 = int8;
+  using s16 = int16;
+  using i16 = int16;
+  using s32 = int32;
+  using i32 = int32;
+  using s64 = int64;
+  using i64 = int64;
+
+} // namespace corelib
+
+#endif // __CORELIB_TYPEDEF_HPP__
