0e3add4b95e6d12effb5f3a153621e6ec05ed009
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2025-02-12 18:36:45 +0100

    add basic print utilities

---
+++ include/corelib/print.hpp
@@ -0,0 +1,24 @@
+#ifndef CORELIB_PRINT_HPP
+#define CORELIB_PRINT_HPP 1
+
+#include <format>
+#include <print>
+#include <utility>
+
+namespace corelib {
+
+    template <typename... Args>
+    void print(::std::format_string<Args..> fmt, Args&&...args)
+    {
+        ::std::print(fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void println(::std::format_string<Args..> fmt, Args&&...args)
+    {
+        ::std::println(fmt, std::forward<Args>(args)...);
+    }
+
+} // namespace corelib
+
+#endif /* CORELIB_PRINT_HPP */
